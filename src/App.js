94a09365b0faaf3bbfa4464b555006ca72f35367
LeahESc <leahemily1@gmail.com>
2021-03-16 13:50:29 -0700

    adds navbar to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import Home from './Components/Home'
 import About from './Components/About'
 import Work from './Components/Work'
+import Navbar from './Components/Navbar';
 
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
         </p>
       </header>
     </div> */}
+    <Navbar />
     <Route exact path='/' component={Home} />
     <Route exact path='/about' component={About} />
     <Route exact path='/work' component={Work} />
