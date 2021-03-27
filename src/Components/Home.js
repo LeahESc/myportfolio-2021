176ec63c5914482f2604daa32e701199ce3a430f
LeahESc <leahemily1@gmail.com>
2021-03-27 14:26:54 -0700

    adds cute buttons with animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'
 import pho from '../images/pho.png'
 import TechSkills from './TechSkills'
-import phomovie from '../images/phomovie.mp4'
-import {Grid, Column} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 import Footer from './Footer'
 
 class Home extends Component {
     
     render() {
         return (
+            <>
             <div className="intro-header">
-            <div className="intro-text">
+                <div className="intro">
 
-                <h1 className="intro--title">
-                    <span className="intro--title--first "> Hi,</span>
-                    <span className="intro--title--second"> I'm Leah</span>
-                </h1>
+                    <h1 className="intro--title">
+                        <span className="intro--title--first "> Hi,</span>
+                        <span className="intro--title--second"> I'm Leah</span>
+                    </h1>
 
-                <div className="intro--subtitle">
-                        Full Stack Software Engineer + lover of pho 
+                    <div className="intro--subtitle">
+                            Full Stack Software Engineer + lover of pho 
+                    </div>
+                    
+                    <Link to="/about">
+                     <button className="btn btn-home">About</button>
+                    </Link>
+                    <Link to="/work">
+                     <button className="btn btn-home">Work</button>
+                    </Link>
+                    {/* <Link to="/technical skills">
+                     <button className="btn btn-home">Technical Skills</button>
+                    </Link> */}
+
                 </div>
+            
+               
+            
             </div>
+            <>
                 <Footer />
-            </div>
+           </>
+           </>
         )
     }
 }
